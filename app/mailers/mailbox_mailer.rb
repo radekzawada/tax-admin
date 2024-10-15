3209166bcdc93ec62c4d03c16787d093eb2b9e9b
Radek <radek_zawada@op.pl>
2024-10-15 22:53:03 +0200

    Add translations

--- app/mailers/mailbox_mailer.rb
+++ app/mailers/mailbox_mailer.rb
@@ -1,29 +1,29 @@
 class MailboxMailer < ApplicationMailer
-# Temat: Informacja o podatkach
+  # Temat: Informacja o podatkach
 
-# Dzień dobry,
+  # Dzień dobry,
 
-# Podatki za wskazany okres wyglądają następująco:
+  # Podatki za wskazany okres wyglądają następująco:
 
-# Rodzaj podatku: %rodzaj podatku np. PIT-36L, PIT-36, PIT-28%
-# Okres: %okres, np. "maj 2024" lub "3. kwartał"%
-# Termin płatności: %termin płatności, data%
-# Kwota: %kwota podatku%
+  # Rodzaj podatku: %rodzaj podatku np. PIT-36L, PIT-36, PIT-28%
+  # Okres: %okres, np. "maj 2024" lub "3. kwartał"%
+  # Termin płatności: %termin płatności, data%
+  # Kwota: %kwota podatku%
 
-# %opcjonalnie%
-# Rodzaj podatku: VAT-7 lub VAT-7K
-# Okres: %okres, np. "maj 2024" lub "3. kwartał"%
-# Termin płatności: %termin płatności, data%
-# Kwota: %kwota podatku%
+  # %opcjonalnie%
+  # Rodzaj podatku: VAT-7 lub VAT-7K
+  # Okres: %okres, np. "maj 2024" lub "3. kwartał"%
+  # Termin płatności: %termin płatności, data%
+  # Kwota: %kwota podatku%
 
-# Numer konta do wpłaty (dla wszystkich podatków): %numer konta do wpłaty podatku%
+  # Numer konta do wpłaty (dla wszystkich podatków): %numer konta do wpłaty podatku%
 
-# W razie pytań prosimy o kontakt.
+  # W razie pytań prosimy o kontakt.
 
-# Z poważaniem,
-# %Twoje Biuro Rachunkowe%
+  # Z poważaniem,
+  # %Twoje Biuro Rachunkowe%
   def tax_information_message(record)
     @message = message
-    mail(to: @message.mailbox.email, subject: 'You have a new message')
+    mail(to: @message.mailbox.email, subject: "You have a new message")
   end
 end
