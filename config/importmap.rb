dad84e4ec8218168d945e2eafdd5395b84ca6fdb
Radek <radek_zawada@op.pl>
2024-10-23 15:15:00 +0200

    Expose modal form for other js components

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
+pin "modal_form"
 pin "popper", to: "popper.js", preload: true
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
