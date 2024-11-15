efa37f201a99fb9abf68d15bccfa2cf10a0b8e1d
Radek <radek_zawada@op.pl>
2024-11-15 22:43:34 +0100

    Add validation to template configuration

--- app/models/message_template/configuration.rb
+++ app/models/message_template/configuration.rb
@@ -14,6 +14,10 @@ class MessageTemplate::Configuration < Dry::Struct
     def validatable?
       false
     end
+
+    def income_variable?
+      false
+    end
   end
 
   class Gap < Cell
@@ -22,17 +26,22 @@ class MessageTemplate::Configuration < Dry::Struct
 
   class Header < Cell
     attribute :label, Types::Symbol
-    attribute :variable?, Types::Symbol
+    attribute :variable?, Types::Coercible::TemplateVariable
     attribute :type, Types::Symbol.default(:string)
     attribute :options?, Types::Array.of(Types::String)
 
     def validatable?
       type == :enum
     end
+
+    def income_variable?
+      variable.to_h[:type] == :income
+    end
   end
 
   attribute :rows, Types::Array.of(Types::Array.of(Types::Instance(Header) | Types::Instance(Gap)))
   attribute :data_start_row, Types::Integer
+  attribute :validations, Types::Interface(:call)
 
   def self.from_hash(rows:, **data)
     new(
@@ -44,6 +53,20 @@ class MessageTemplate::Configuration < Dry::Struct
   end
 
   def columns_count
-    rows.map { |row| row.sum(&:col_span) }.max
+    @income_variables ||= rows.map { |row| row.sum(&:col_span) }.max
+  end
+
+  def income_variables
+    @income_variables ||= rows.each_with_object({}).with_index do |(row, result), row_index|
+      col_index = 0
+
+      row.each do |config|
+        if config.income_variable?
+          variable_name = config.variable[:name]
+          result[col_index] = variable_name
+        end
+        col_index += config.col_span
+      end
+    end.compact.sort.to_h
   end
 end
--- spec/models/message_template/configuration_spec.rb
+++ spec/models/message_template/configuration_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe MessageTemplate::Configuration, type: :model do
   describe ".from_hash" do
-    subject(:from_hash) { described_class.from_hash(rows:, data_start_row:) }
+    subject(:from_hash) { described_class.from_hash(rows:, data_start_row:, validations:) }
 
     let(:rows) do
       [
@@ -29,16 +29,17 @@ RSpec.describe MessageTemplate::Configuration, type: :model do
       ]
     end
     let(:data_start_row) { 2 }
+    let(:validations) { Proc.new { } }
 
     it "creates a new Configuration instance from a hash" do
       expect(from_hash).to be_a(described_class) && have_attributes(
         rows: contain_exactly(
           include(
             an_instance_of(MessageTemplate::Configuration::Header) & have_attributes(
-              label: :full_name, col_span: 1, row_span: 2, variable: :full_name
+              label: :full_name, col_span: 1, row_span: 2, variable: { name: :full_name, type: :income }
             ),
             an_instance_of(MessageTemplate::Configuration::Header) & have_attributes(
-              label: :email, col_span: 1, row_span: 2, variable: :email
+              label: :email, col_span: 1, row_span: 2, variable: { name: :email, type: :income }
             ),
             an_instance_of(MessageTemplate::Configuration::Header) & have_attributes(
               label: :pit, col_span: 4
@@ -47,7 +48,7 @@ RSpec.describe MessageTemplate::Configuration, type: :model do
               label: :vat, col_span: 4
             ),
             an_instance_of(MessageTemplate::Configuration::Header) & have_attributes(
-              label: :account_number, col_span: 1, row_span: 2, variable: :account_number
+              label: :account_number, col_span: 1, row_span: 2, variable: { name: :account_number, type: :income }
             )
           ),
           include(
@@ -58,32 +59,35 @@ RSpec.describe MessageTemplate::Configuration, type: :model do
               displayable: false, col_span: 1, row_span: 1
             ),
             an_instance_of(MessageTemplate::Configuration::Header) & have_attributes(
-              label: :tax_type, variable: :income_tax_type, type: :enum, options: %w[PIT-36L PIT-36 PIT-28]
+              label: :tax_type, variable: { name: :income_tax_type, type: :income }, type: :enum,
+              options: %w[PIT-36L PIT-36 PIT-28]
             ),
             an_instance_of(MessageTemplate::Configuration::Header) & have_attributes(
-              label: :period, variable: :income_period
+              label: :period, variable: { name: :income_period, type: :income }
             ),
             an_instance_of(MessageTemplate::Configuration::Header) & have_attributes(
-              label: :payment_deadline, variable: :income_payment_deadline
+              label: :payment_deadline, variable: { name: :income_payment_deadline, type: :income }
             ),
             an_instance_of(MessageTemplate::Configuration::Header) & have_attributes(
-              label: :tax_amount, variable: :income_tax_amount
+              label: :tax_amount, variable: { name: :income_tax_amount, type: :income }
             ),
             an_instance_of(MessageTemplate::Configuration::Header) & have_attributes(
-              label: :tax_type, variable: :value_added_tax_type, type: :enum, options: %w[VAT-7 VAT-7K]
+              label: :tax_type, variable: { name: :value_added_tax_type, type: :income },
+              type: :enum, options: %w[VAT-7 VAT-7K]
             ),
             an_instance_of(MessageTemplate::Configuration::Header) & have_attributes(
-              label: :period, variable: :value_added_period
+              label: :period, variable: { name: :value_added_period, type: :income }
             ),
             an_instance_of(MessageTemplate::Configuration::Header) & have_attributes(
-              label: :payment_deadline, variable: :value_added_payment_deadline
+              label: :payment_deadline, variable: { name: :value_added_payment_deadline, type: :income }
             ),
             an_instance_of(MessageTemplate::Configuration::Header) & have_attributes(
-              label: :tax_amount, variable: :value_added_tax_amount
+              label: :tax_amount, variable: { name: :value_added_tax_amount, type: :income }
             )
           )
         ),
-        data_start_row: 2
+        data_start_row: 2,
+        validations:
       )
     end
   end
