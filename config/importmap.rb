2188b48e9e760cbf2ad149e9065a0fca172d2d8e
Radek <radek_zawada@op.pl>
2024-11-20 21:41:31 +0100

    Fix js vurnelability

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,7 +4,7 @@ pin "application"
 pin "modal_form"
 pin "popper", to: "popper.js", preload: true
 pin "bootstrap", to: "bootstrap.min.js", preload: true
-pin "axios", to: "https://cdn.jsdelivr.net/npm/axios@1.4.0/dist/axios.min.js", preload: true
+pin "axios", to: "https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
